2348b0da9c143f04360b06c5929dad5a90fb35f3
iamthemuffinman <robertjdeusser@gmail.com>
2016-11-21 15:57:36 -0500

    Add helper functions for receiving pointer values

--- katello/katello.go
+++ katello/katello.go
@@ -140,3 +140,15 @@ func addOptions(s string, opt interface{}) (string, error) {
 	u.RawQuery = qs.Encode()
 	return u.String(), nil
 }
+
+// Bool is a helper routine that allocates a new bool value
+// to store v and returns a pointer to it.
+func Bool(v bool) *bool { return &v }
+
+// Int is a helper routine that allocates a new int value
+// to store v and returns a pointer to it.
+func Int(v int) *int { return &v }
+
+// String is a helper routine that allocates a new string value
+// to store v and returns a pointer to it.
+func String(v string) *string { return &v }
