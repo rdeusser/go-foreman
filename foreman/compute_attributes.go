14be5e87b0c841fe358be74943f8bf071dcf8938
iamthemuffinman <robertjdeusser@gmail.com>
2016-12-23 21:14:07 -0500

    Move types to their correct places

--- foreman/compute_attributes.go
+++ foreman/compute_attributes.go
@@ -1,1 +1,14 @@
 package foreman
+
+// ComputeAttributes represents the attributes of a host.
+type ComputeAttributes struct {
+	CPUs               *int    `json:"cpus,omitempty"`
+	Cores              *int    `json:"corespersocket,omitempty"`
+	Memory             *int    `json:"memory_mb,omitempty"`
+	Cluster            *string `json:"cluster,omitempty"`
+	Path               *string `json:"path,omitempty"`
+	GuestID            *string `json:"guest_id,omitempty"`
+	SCSIControllerType *string `json:"scsi_controller_type,omitempty"`
+	HardwareVersion    *string `json:"hardware_version,omitempty"`
+	Start              *bool   `json:"start,omitempty"`
+}
--- foreman/hosts.go
+++ foreman/hosts.go
@@ -45,19 +45,6 @@ func (h Host) String() string {
 	return Stringify(h)
 }
 
-// ComputeAttributes represents the attributes of a host.
-type ComputeAttributes struct {
-	CPUs               *int    `json:"cpus,omitempty"`
-	Cores              *int    `json:"corespersocket,omitempty"`
-	Memory             *int    `json:"memory_mb,omitempty"`
-	Cluster            *string `json:"cluster,omitempty"`
-	Path               *string `json:"path,omitempty"`
-	GuestID            *string `json:"guest_id,omitempty"`
-	SCSIControllerType *string `json:"scsi_controller_type,omitempty"`
-	HardwareVersion    *string `json:"hardware_version,omitempty"`
-	Start              *bool   `json:"start,omitempty"`
-}
-
 // InterfaceAttributes represents the attributes of a hosts' interface(s).
 type InterfaceAttributes struct {
 	Mac               *string            `json:"mac,omitempty"`
